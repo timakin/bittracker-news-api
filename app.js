13178938d28d9ce19c3e906909d2202a9733c793
timakin <timaki.st@gmail.com>
2015-09-24 00:53:54 +0900

    [fix] api call

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var bodyParser   = require('body-parser');
+var news         = require('./routes/news');
+var app          = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/api/v1/news', news);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,28 +22,10 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+    res.status(err.status || 500).json({
+        message: err.message
     });
 });
-
 
 module.exports = app;
---
+++ routes/news.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var Promise = require('promise');
+var request = require('superagent');
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  var targetUrls = [
+  	"http://cloud.feedly.com/v3/mixes/contents?streamId=feed%2Fhttp%3A%2F%2Fbtcnews.jp%2Ffeed%2F&count=10",
+  	"http://cloud.feedly.com/v3/mixes/contents?streamId=feed%2Fhttp%3A%2F%2Fnews.bitflyer.jp%2Ffeed%2F&count=10"
+  ]
+
+  var promises = [];
+  for (var i = 0; i < targetUrls.length; i++) {
+  	promises[i] = new Promise(function (resolve, reject){
+    	request(targetUrls[i], function(err, result) {
+    		if (err) reject(err);
+          	resolve(result);
+        });
+    });
+  };
+
+  Promise.all(promises)
+  	.then(function(results) {
+  		return results.concat();
+  	})
+  	.then(function(results) {
+  		res.json({
+  			response: results
+  		});
+  	});
+});
+
+module.exports = router;
