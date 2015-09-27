99bdd829e6b478d76e3e7ec99d1f01eee35e97a4
timakin <timaki.st@gmail.com>
2015-09-27 09:51:08 +0900

    [fix] model

--- controllers/news.js
+++ controllers/news.js
@@ -9,19 +9,11 @@ var requestPromise = require('../utils/requestPromise');
 
 /* GET users listing. */
 router.get('/jp', function(req, res) {
-  var promises = [];
-  for (var i = 0; i < endpoints.feedly.jp.length; i++) {
-    promises[i] = requestPromise(endpoints.feedly.jp[i], function(res) {
-        return newsModel.getNewsItems(res.body.items);
+  newsModel.getNews(function(news) {
+    res.json({
+      response: newsModel.sortByCreatedAt(news)
     });
-  }
-
-  Promise.all(promises)
-    .then(function(results) {
-      res.json({
-        response: newsModel.getSortedNews(results)
-      });
-    });
+  });
 });
 
 module.exports = router;
--- models/news.model.js
+++ models/news.model.js
@@ -1,24 +1,40 @@
-var _ = require('underscore');
+var _              = require('underscore');
+var endpoints      = require('../config/endpoints.json');
+var Promise        = require('promise');
+var requestPromise = require('../utils/requestPromise');
+
+var filterNewsItems = function(items) {
+  var news = [];
+  _.map(items, function(item) {
+    if (item.visual && item.visual.url != 'none') {
+      news.push({
+        title:      item.title,
+        url:        item.originId,
+        origin:     item.origin.title,
+        image_uri:  item.visual.url,
+        created_at: item.published
+      });
+    }
+  });
+  return news;
+};
 
 module.exports = {
-  getNewsItems: function(items) {
-      var news = [];
-    _.map(items, function(item) {
-      if (item.visual && item.visual.url != 'none') {
-        news.push({
-          title:      item.title,
-          url:        item.originId,
-          origin:     item.origin.title,
-          image_uri:  item.visual.url,
-          created_at: item.published
-        });
-      }
-    });
-    return news;
-  },
-  getSortedNews: function(items) {
+  sortByCreatedAt: function(items) {
     var news = [];
     _.map(items, function(item) { news = news.concat(item); });
     return _.sortBy(news, 'created_at').reverse();
+  },
+  getNews: function(cb) {
+    var promises = [];
+    for (var i = 0; i < endpoints.feedly.jp.length; i++) {
+      promises[i] = requestPromise(endpoints.feedly.jp[i], function(res) {
+          return filterNewsItems(res.body.items);
+      });
+    }
+    Promise.all(promises)
+      .then(function(news) {
+        cb(news);
+      });
   }
 };
